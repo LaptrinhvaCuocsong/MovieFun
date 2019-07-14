b74ea3604c962ad35c91771274fffcd7ad347bc9
Pied Piper <kasavavava1998@gmail.com>
2019-07-14 23:48:10 +0700

    kaka

--- MovieFun/MovieFun/View/FavoriteMovie/FavoriteMovieController.swift
+++ MovieFun/MovieFun/View/FavoriteMovie/FavoriteMovieController.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+class FavoriteMovieController {
+    
+    var favoriteMovieViewModel: FavoriteMovieViewModel?
+    
+    init() {
+        favoriteMovieViewModel = FavoriteMovieViewModel()
+    }
+    
+    func start() {
+        favoriteMovieViewModel!.isFetching?.value = true
+        MovieService.share.fetchFavoriteMovie {[weak self] (movies) in
+            self?.buildViewModels(movies: movies)
+            self?.favoriteMovieViewModel?.isFetching?.value = false
+        }
+    }
+    
+    func buildViewModels(movies: [Movie]?) {
+        if let favoriteMovies = movies {
+            let favoriteSectionVM = FavoriteSectionViewModel()
+            favoriteMovieViewModel!.sectionViewModels!.value!.append(favoriteSectionVM)
+            for (_, movie) in favoriteMovies.enumerated() {
+                let favoriteRowVM = FavoriteRowViewModel(movie: DynamicType<Movie>(value: movie))
+                favoriteSectionVM.rowViewModels!.value!.append(favoriteRowVM)
+            }
+        }
+    }
+    
+}
